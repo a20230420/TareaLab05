d4753794236502355a27e063bae5628608bfed40
Adrian <a20230420@pucp.edu.pe>
2026-05-09 08:33:07 -0500

    inicial - estructura del proyecto

---
+++ src/test/java/com/example/tarealab05/TareaLab05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarealab05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaLab05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
